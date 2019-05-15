3d41392c13b63f1b9a2ef9ecee4d5a64cc251400
mariageorgeamin <mariageorgeamin@yahoo.com>
2019-05-15 15:08:13 +0200

    using mysql instead of sqlite

--- HL7Broker/settings.py
+++ HL7Broker/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'HL7Broker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',  # databse name
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # default host
+        'PORT': '3306'  # default port
     }
 }
 
