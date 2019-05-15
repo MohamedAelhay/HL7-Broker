ca33d1c8c35641e7e253c5c42993d338eb044a59
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-05-15 22:21:10 +0200

    add call_hl7_director

--- hl7parser/director.py
+++ hl7parser/director.py
@@ -30,9 +30,15 @@ json_msg = {
     }
 }
 
-first_json = MessagePreparer(json_msg)
-message_creator = MessageCreator(first_json.get_array_of_data_dictionaries(), '2.5')
-message_creator.create_msh_segment().creat_pid_segment().create_pv1_segment()
-print(first_json.get_patient_data())
-print(first_json.get_array_of_data_dictionaries())
-print(message_creator.get_hl7_message().children)
+
+def call_hl7_director(json_message):
+
+    first_json = MessagePreparer(json_message)
+    message_creator = MessageCreator(first_json.get_array_of_data_dictionaries(), '2.5')
+    message_creator.create_msh_segment().creat_pid_segment().create_pv1_segment()
+    print(first_json.get_patient_data())
+    print(first_json.get_array_of_data_dictionaries())
+    return message_creator.get_hl7_message()
+
+
+print(call_hl7_director(json_msg).children)
