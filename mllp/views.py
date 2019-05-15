2256fc7b2154ea73256ba0c3a6f9b3dc1943871f
MohamedAbdelhay <cap.mohamed.abdelhay@gmail.com>
2019-05-15 18:50:10 +0200

    Add Mllp_Server & Command

--- mllp/views.py
+++ mllp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from hl7apy.mllp import AbstractHandler, AbstractErrorHandler, UnsupportedMessageType
+from hl7apy.parser import parse_message
 
 # Create your views here.
+
+class PDQHandler(AbstractHandler):
+    def reply(self):
+        print("I'm Listening... ")
+        msg = parse_message(self.incoming_message)
+        # Do Something with the Message
+
+        res = Message('RSP_K21')
+        # Populate the Message
+
+        return res.to_mllp()
+
+
+class ErrorHandler(AbstractErrorHandler):
+    def reply(self):
+        if isinstance(self.exc, UnsupportedMessageType):
+            # Return custom response for unsupported message
+            print("Unsupported Message Type")
+        else:
+            # Return custom response for general errors
+            print("General Error")
