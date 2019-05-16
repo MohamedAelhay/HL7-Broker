dc849ea3bd9b324c3cab0fb0fe5e6dd9c4767481
mahydit <mahydit_anber@outlook.com>
2019-05-16 15:04:14 +0200

    create dict for request json

--- hl7parser/director.py
+++ hl7parser/director.py
@@ -1,44 +1,9 @@
 from hl7parser.creator import MessageCreator
 from hl7parser.preparer import MessagePreparer
 
-json_msg = {
-
-    'meta_data': {
-
-        'te': 'ADT',
-        'scope': 'A01',
-        'device': 'ultraSonic'
-    },
-
-    'data': {
-
-        'patient': {
-            'first_name': 'Ahmed',
-            'last_name': 'mahmoud',
-            'date_of_birth': '1994-02-13',
-            'mobile': '01111861133',
-            'address': 'agamy-alex'
-        },
-
-        'visit': {
-            'visit_id': "1",
-            'preadmit_number': "1234",
-            'consulting_doctor_first_name': "Mohamed",
-            'consulting_doctor_last_name': "Aly",
-
-        }
-    }
-}
-
-
 def call_hl7_director(json_message):
 
     first_json = MessagePreparer(json_message)
     message_creator = MessageCreator(first_json.get_array_of_data_dictionaries(), '2.5')
     message_creator.create_msh_segment().creat_pid_segment().create_pv1_segment()
-    print(first_json.get_patient_data())
-    print(first_json.get_array_of_data_dictionaries())
     return message_creator.get_hl7_message()
-
-
-print(call_hl7_director(json_msg).children)
