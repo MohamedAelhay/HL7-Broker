689fa04f8921ea71815e816a43bac46771035e8f
mahydit <mahydit_anber@outlook.com>
2019-05-18 00:30:40 +0200

    add device_details and parsing endpoint

--- HL7Broker/urls.py
+++ HL7Broker/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.urls import path
-from middleware import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^test/$',views.getRequest,name="request")
+    path('api/',include('middleware.urls'))
 ]
--- middleware/views.py
+++ middleware/views.py
@@ -1,11 +1,56 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import requests
+from django.http import HttpResponse, JsonResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
+from middleware.models import Device, TriggerEvent, Scope, Segment, Field
+from middleware.serializers import DeviceSerializer
 from hl7parser.director import call_hl7_director
 
-# Create your views here.
-def getRequest(x):
-    response = requests.get('https://api.myjson.com/bins/xgql6', verify=False)
-    data = response.json()
-    print(call_hl7_director(data).children)
-    return HttpResponse("")
+
+@api_view(['GET'])
+def device_details(request, pk):
+    try:
+        device = Device.objects.get(pk=pk)
+    except Device.DoesNotExist:
+        return Response("Not found", status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = DeviceSerializer(device)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def parse_request(request):
+    data = JSONParser().parse(request)
+    if is_request_valid(data):
+        print(call_hl7_director(data).children)
+        return Response(data, status=status.HTTP_200_OK)
+    else:
+        return Response("Bad request", status=status.HTTP_400_BAD_REQUEST)  
+
+def is_request_valid(data):
+    if all(elem in data["meta_data"].keys() for elem in ["te","device","scope"]):
+        if is_device_valid(data["meta_data"]["device"]) and is_capability_valid(data):
+            return True
+    return False
+
+def is_device_valid(pk):
+    try:
+        device = Device.objects.get(pk=pk)
+    except Device.DoesNotExist:
+        return False
+    return True
+
+def is_capability_valid(data):
+    device = Device.objects.get(pk=data["meta_data"]["device"])
+    te = TriggerEvent.objects.get(device=device, code=data["meta_data"]["te"])
+    scope = Scope.objects.get(trigger_event=te, code=data["meta_data"]["scope"])
+    segments = Segment.objects.filter(scope=scope)
+    for segment in segments:
+        if segment.name not in data["data"].keys():
+            return False
+        else:
+            fields = Field.objects.filter(segment=segment)
+            for field in fields:
+                if field.name not in data["data"][segment.name]:
+                    return False
+    return True
