bf899113287e7364e690d763cbe0c97b8e602323
AbdelRahman-Elshafai <abdelrahman.elshafai@gmail.com>
2019-05-29 23:11:44 +0200

    create logger class

--- hl7parser/logger.py
+++ hl7parser/logger.py
@@ -5,6 +5,6 @@ class Logger:
     @staticmethod
     def log(response , client):
         time = datetime.datetime.strftime(datetime.datetime.now(),"%Y-%m-%dT%H:%M:%S")
-        Log.objects.create(response = response, time= time, client_id= client)
+        Log.objects.create(response = response, time= time, client_id= client.id)
 
 
