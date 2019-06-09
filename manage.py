bb2d170cd1cd680b251e66694acc60aacb72937a
YousraEssam <yousraa.essam.15@hotmail.com>
2019-06-10 01:08:05 +0200

    fic .env

--- manage.py
+++ manage.py
@@ -3,9 +3,11 @@ import os
 import sys
 
 import dotenv
+from dotenv import load_dotenv
+load_dotenv()
 
 if __name__ == '__main__':
-    dotenv.read_dotenv()
+    # dotenv.read_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HL7Broker.settings')
     try:
         from django.core.management import execute_from_command_line
