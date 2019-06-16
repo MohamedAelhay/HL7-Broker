ea1c2b40d247d9005189902757681aeb289ec1f8
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-06-16 15:54:05 +0200

    Create hl7_ack_serializer

---
+++ hl7parser/hl7_ack_serializer.py
@@ -0,0 +1,21 @@
+class Hl7AckSerializer:
+
+    def __init__(self, ack=None, device_name=None):
+        self.__ack = ack
+        self.__device_name = device_name
+        self.__hl7_ack_dict = {}
+
+    def __check_ack_status(self):
+        if "AA" in self.__ack:
+            return "Acceptance"
+        if "AE" in self.__ack:
+            return "Error"
+        if "AR" in self.__ack:
+            return "Rejection"
+
+    def __serialize_ack(self):
+        self.__hl7_ack_dict["status"] = self.__check_ack_status()
+        self.__hl7_ack_dict["device"] = self.__device_name
+
+    def get_serialized_hl7_ack(self):
+        return self.__hl7_ack_dict
